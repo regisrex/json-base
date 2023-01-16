cf15c53299d4c99ae90709a1ee42d9ece91b3e14
ndzhwr <ndizihiweregis06@gmail.com>
2023-01-16 10:40:22 +0200

    feat : limit

--- index.ts
+++ index.ts
@@ -3,9 +3,10 @@ import { getData } from "./scripts/getData.js";
 (async function () {
     const data = await getData({
         collection: "users",
-        where: {
+        where : {
             id : 1
-        }
+        },
+        limit : 1
     }
     )
     console.log(data)
--- scripts/getData.ts
+++ scripts/getData.ts
@@ -1,28 +1,35 @@
 import { MissingParamError, NotFoundError } from "../errors/errorHandler.js"
 import { getCollection } from "../utils/getCollection.js"
-export  interface GetDataOptions {
-    where: any,
+export interface GetDataOptions {
+    where?: any,
     limit?: number
 }
-export  interface GetDataProps extends GetDataOptions { 
+export interface GetDataProps extends GetDataOptions {
     collection: string
 }
 
-export async function getData(params : GetDataProps): Promise<any> {
-    if(!params.collection){
+export async function getData(params: GetDataProps): Promise<any> {
+    let data_to_return: any;
+    if (!params.collection) {
         throw new MissingParamError("collection")
     }
     const collection = await getCollection(params.collection)
-    if(params){
-        const where_data   = Object.entries(params.where)
-        for(let i  : number = 0 ; i  < where_data.length ; i++){
-         const doc : any  =  collection.find((collection :  any) => collection[`${where_data[i][0]}`] == `${where_data[i][1]}`)
-            if(doc)
-                return doc
+    data_to_return = collection
+    if (params.where) {
+        const where_data = Object.entries(params.where)
+        for (let i: number = 0; i < where_data.length; i++) {
+            const data: any = data_to_return.filter((collection: any) => collection[`${where_data[i][0]}`] == `${where_data[i][1]}`)
+            if (data)
+                data_to_return = data
             else
-                return new NotFoundError(`Datum with '${where_data[i][0]}' set to '${where_data[i][1]}' was not found in '${params.collection}' Collection`)
-        } 
-    }else{
-        return collection
+                throw new NotFoundError(`Datum with '${where_data[i][0]}' set to '${where_data[i][1]}' was not found in '${params.collection}' Collection`)
+        }
     }
+    if (params.limit) {
+        
+        if (data_to_return && data_to_return.length != 1)
+            data_to_return = (data_to_return as any[]).slice(0, params.limit)
+    }
+
+    return data_to_return
 }
