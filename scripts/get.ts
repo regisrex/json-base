144b8d048218881cabd66f425ec6b767c4b8ddc7
M.Pac <pacifiquemurangwa001@gmail.com>
2023-04-24 09:57:39 +0200

    added: lt, gt, lte, gte functionalites

--- scripts/get.ts
+++ scripts/get.ts
@@ -1,26 +1,22 @@
-import { MissingParamError, NotFoundError } from "../errors/errorHandler.js"
-import { getCollection } from "../utils/getCollection.js"
+import { MissingParamError, NotFoundError } from "../errors/errorHandler.js";
+import { getCollection } from "../utils/getCollection.js";
 
 export interface GetDataOptions {
-    where?: any,
-    limit?: number,
-    ls?: any,
-    gt?: any
+  where?: any;
+  limit?: number;
 }
 
 export interface GetDataProps extends GetDataOptions {
-    collection: string
+  collection: string;
 }
 
 /**
  * ### get()
- * An asynchronous json-base API to get data from database.json
+ * An asyncronous json-base api to get data from database.json
  *  
  * @param params.collection A collection to get data from 
- * @param params.where A where clause like SQL's WHERE, (optional)
- * @param params.limit A limit of records to retrieve, (optional)
- * @param params.ls A filter to retrieve records where the specified field is less than or equal to the specified value, (optional)
- * @param params.gt A filter to retrieve records where the specified field is greater than or equal to the specified value, (optional)
+ * @param params.where  A where clause like SQL's WHERE , (optional)
+ * @param params.limit A Limit of record to retreive , (optional)
  * @example
  * ```
  * import { get } from 'json-base'
@@ -28,52 +24,76 @@ export interface GetDataProps extends GetDataOptions {
  *      const aUsersWithId2  =  await get({
  *              collection : "users",
  *              where : {
- *                    id : 2
+ *                    id : { lt: 10, gt: 3 }
  *                  },
  *              limit :  1
- *              ls: 5
- *              gt: 2
  *          })
  * }())
  * ```
  */
 export async function get(params: GetDataProps): Promise<any> {
-    if (!params.collection) {
-        throw new MissingParamError("collection")
-    }
-    const collection = await getCollection(params.collection)
-
-    let data_to_return: any = collection
-
-    if (params.where) {
-        const where_data = Object.entries(params.where)
-        for (let i: number = 0; i < where_data.length; i++) {
-            const data: any = data_to_return.filter((collection: any) => collection[`${where_data[i][0]}`] == `${where_data[i][1]}`)
-            if (data.length > 0) {
-                data_to_return = data
-            } else {
-                throw new NotFoundError(`Datum with '${where_data[i][0]}' set to '${where_data[i][1]}' was not found in '${params.collection}' Collection`)
-            }
+  let data_to_return: any;
+  if (!params.collection) {
+    throw new MissingParamError("collection");
+  }
+  const collection = await getCollection(params.collection);
+  data_to_return = collection;
+  if (params.where) {
+    const where_data = Object.entries(params.where);
+    for (let i: number = 0; i < where_data.length; i++) {
+      const [field, filter]:[any, any] = where_data[i];
+      if (typeof filter === "object") {
+        const filterEntries = Object.entries(filter);
+        let data: any = data_to_return;
+        for (let j = 0; j < filterEntries.length; j++) {
+          const [operator, value] = filterEntries[j];
+          switch (operator) {
+            case "lt":
+              data = data.filter(
+                (item: any) => item[field] < Number(value)
+              );
+              break;
+            case "lte":
+              data = data.filter(
+                (item: any) => item[field] <= Number(value)
+              );
+              break;
+            case "gt":
+              data = data.filter(
+                (item: any) => item[field] > Number(value)
+              );
+              break;
+            case "gte":
+              data = data.filter(
+                (item: any) => item[field] >= Number(value)
+              );
+              break;
+            case "eq":
+              data = data.filter(
+                (item: any) => item[field] === Number(value)
+              );
+              break;
+            default:
+              throw new Error(`Operator '${operator}' not supported.`);
+          }
         }
+        data_to_return = data;
+      } else {
+        const data: any = data_to_return.filter(
+          (collection: any) => collection[`${field}`] == `${filter}`
+        );
+        if (data) data_to_return = data;
+        else
+          throw new NotFoundError(
+            `Datum with '${field}' set to '${filter}' was not found in '${params.collection}' Collection`
+          );
+      }
     }
+  }
+  if (params.limit) {
+    if (data_to_return && data_to_return.length != 1)
+      data_to_return = (data_to_return as any[]).slice(0, params.limit);
+  }
 
-    if (params.ls) {
-        const ls_data = Object.entries(params.ls)
-        for (let i: number = 0; i < ls_data.length; i++) {
-            data_to_return = data_to_return.filter((collection: any) => collection[`${ls_data[i][0]}`] <= `${ls_data[i][1]}`)
-        }
-    }
-
-    if (params.gt) {
-        const gt_data = Object.entries(params.gt)
-        for (let i: number = 0; i < gt_data.length; i++) {
-            data_to_return = data_to_return.filter((collection: any) => collection[`${gt_data[i][0]}`] >= `${gt_data[i][1]}`)
-        }
-    }
-
-    if (params.limit) {
-        data_to_return = (data_to_return as any[]).slice(0, params.limit)
-    }
-
-    return data_to_return
+  return data_to_return;
 }
